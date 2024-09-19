adefa4db3af0e7e956386e4f17d5a590a75e719b
Jeison <hi@jeisonroblero.ga>
2024-09-19 02:27:54 -0600

    Libraries Optimization Added

--- api/vendor/autoload.php
+++ api/vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit566b298913829ffc1df98fbece4f9631::getLoader();
+return ComposerAutoloaderInitaee306602b5316b34cba5707b6407dc4::getLoader();
